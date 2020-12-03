406dd7bcf16897f581fb433f12c19d458b26e427
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2020-12-03 16:40:00 +0100

    Sound Command Modul Test

---
+++ OgerBot/Modules/SoundCommands.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordOgerBotWeb.Modules
+{
+    public class SoundCommands : ModuleBase<SocketCommandContext>
+    {
+        private readonly string _soundPath = Path.GetFullPath(
+            Path.Combine(AppContext.BaseDirectory, "../OgerBot/sounds"));
+
+
+        private readonly string _videoPath = Path.GetFullPath(
+            Path.Combine(AppContext.BaseDirectory, "../OgerBot/videos"));
+
+
+        [Command("alarm")]
+        [Alias("wiwi")]
+        public async Task SendWiwiw()
+        {
+            await Context.Channel.SendFileAsync(_soundPath + "/wiwiwiwiiw.mp3", embed: Controller.OgerBot.GetStandartSoundEmbed());
+        }
+
+    }
+}
