3826c8f578a3b94417e33c9b6deefdf4b374b8c0
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2020-12-11 20:31:55 +0100

    Added first images

---
+++ OgerBot/Modules/SoundCommands - Copy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordOgerBotWeb.Modules
+{
+    public class ImageCommands : ModuleBase<SocketCommandContext>
+    {
+        private readonly string _imagePath = Path.GetFullPath(
+            Path.Combine(AppContext.BaseDirectory, "../OgerBot/images"));
+
+        [Command("nice")]
+        [Alias("nais")]
+        public async Task SendNice()
+        {
+            var rand = new Random();
+            await Context.Channel.SendFileAsync(_imagePath + $"/nais{rand.Next(1,4)}.gif", embed: Controller.OgerBot.GetStandardSoundEmbed());
+        }
+
+        [Command("sport")]
+        [Alias("sportlord")]
+        public async Task SendSportLord()
+        {
+            var rand = new Random();
+            await Context.Channel.SendFileAsync(_imagePath + $"/sportLord{rand.Next(1, 6)}.gif", embed: Controller.OgerBot.GetStandardSoundEmbed());
+        }
+    }
+}
