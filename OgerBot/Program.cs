820619d7fb039623fc4fbf6845f725655aa85c7d
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2020-12-12 16:53:16 +0100

    Fix

--- OgerBot/Program.cs
+++ OgerBot/Program.cs
@@ -31,7 +31,7 @@ namespace DiscordOgerBotWeb
         {
             var url = Environment.GetEnvironmentVariable("DATABASE_URL");
             if (url == null) return;
-            Globals.DataBaseConnection.Host = url.Substring(url.IndexOf('@') + 1, url.LastIndexOf(':') - url.IndexOf('@'));
+            Globals.DataBaseConnection.Host = url.Substring(url.IndexOf('@') + 1, url.LastIndexOf(':') - url.IndexOf('@') - 1);
             Globals.DataBaseConnection.Name = url.Substring(url.LastIndexOf('/') + 1);
             Globals.DataBaseConnection.UserName = url.Substring(11, url.IndexOf(':', 11) - 11);
             Globals.DataBaseConnection.Password = url.Substring(url.IndexOf(':', 11) + 1, url.IndexOf('@') - url.IndexOf(':', 11) - 1);
