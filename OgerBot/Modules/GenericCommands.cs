81fa6ad0b26c26d1a13b48787d2e59417d69035c
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2020-12-12 11:35:51 +0100

    Seperate Commands test

--- OgerBot/Modules/GenericCommands.cs
+++ OgerBot/Modules/GenericCommands.cs
@@ -10,12 +10,13 @@ namespace DiscordOgerBotWeb.Modules
     public class GenericCommands : ModuleBase<SocketCommandContext>
     {
 
+        private Random _rand = new Random();
+
         [Command("help")]
         [Alias("hilfe")]
         public async Task SendHelp()
         {
 
-            var random = new Random();
             var embed = new EmbedBuilder
             {
                 Title = "Meddl Leudde!:metal:"
@@ -24,20 +25,23 @@ namespace DiscordOgerBotWeb.Modules
 
                 .WithDescription(
                     "Ich versuche deutsche Sätze ins Meddlfrängische zu übersetzen! " + Environment.NewLine +
-                    "Eine Discord Nachricht auf Hochdeutsch nervt dich? Kein Problem! **reagiere einfach auf die Nachricht mit :OgerBot: **" + Environment.NewLine +
+                    "Eine Discord Nachricht auf Hochdeutsch nervt dich? Kein Problem! **reagiere einfach auf die Nachricht mit :OgerBot: **" +
                     Environment.NewLine +
-                    "**Der Server braucht ein Emoji mit dem Namen OgerBot!!** (Wende dich an die Server Admins die wissen das ganz bestimmt)" + Environment.NewLine +
+                    Environment.NewLine +
+                    "**Der Server braucht ein Emoji mit dem Namen OgerBot!!** (Wende dich an die Server Admins die wissen das ganz bestimmt)" +
+                    Environment.NewLine +
                     Environment.NewLine +
                     "Die Nachricht kann auch wieder gelöscht werden indem man die Reaction wieder entfernt")
 
                 .AddField("Sounds",
-                "Ich kann auch Sounds abspielen! Für eine Übersicht schreib einfach **og commands**")
+                    "Ich kann auch Sounds abspielen! Für eine Übersicht schreib einfach **og commands**")
 
                 .AddField("Willst du mich auf deinem eigenen Discord Server?",
                     "Das kannst du ganz einfach [hier](https://discord.com/api/oauth2/authorize?client_id=761895612291350538&permissions=383040&scope=bot) machen!")
 
                 .AddField("Weitere Hilfe",
-                    "Sollte ich mal nicht richtig funktionieren, komm ins [DrachenlordKoreaDiscord](https://discord.gg/MmWQ5pCsHa) oder wende dich bitte an meinen Erbauer:" + Environment.NewLine +
+                    "Sollte ich mal nicht richtig funktionieren, komm ins [DrachenlordKoreaDiscord](https://discord.gg/MmWQ5pCsHa) oder wende dich bitte an meinen Erbauer:" +
+                    Environment.NewLine +
                     "[Discord](https://discordapp.com/users/386989432148066306) | [Reddit](https://www.reddit.com/user/MoriPastaPizza)")
 
                 .AddField("Links",
@@ -48,7 +52,7 @@ namespace DiscordOgerBotWeb.Modules
                 .WithAuthor(Context.Client.CurrentUser)
                 .WithFooter(footer =>
                     footer.Text =
-                        Controller.OgerBot.FooterDictionary[random.Next(Controller.OgerBot.FooterDictionary.Count)])
+                        Controller.OgerBot.FooterDictionary[_rand.Next(Controller.OgerBot.FooterDictionary.Count)])
                 .WithColor(Color.Blue)
                 .WithCurrentTimestamp()
                 .Build();
@@ -67,7 +71,9 @@ namespace DiscordOgerBotWeb.Modules
                 .OrderBy(m => m.Name)
                 .ToList();
 
-            var commandList = commands.Select(command => $"**{command.Aliases.Aggregate((i, j) => i + " " + j)}** {command.Summary}").ToList();
+            var commandList = commands
+                .Select(command => $"**{command.Aliases.Aggregate((i, j) => i + " " + j)}** {command.Summary}")
+                .ToList();
             var fieldString = commandList.Aggregate((i, j) => i + " | " + j).ToString();
 
             var embedBuilder = new EmbedBuilder
@@ -90,5 +96,46 @@ namespace DiscordOgerBotWeb.Modules
 
             await Context.Channel.SendMessageAsync(embed: embedBuilder.Build());
         }
+
+        [Command("sounds")]
+        public async Task SendSounds()
+        {
+            var module = Controller.OgerBot.CommandService.Modules
+                .FirstOrDefault(m => m.Name == nameof(SoundCommands));
+
+            if (module == null) return;
+
+            var commands = module.Commands
+                .Where(m => m.Name != "asia")
+                .OrderBy(m => m.Name)
+                .ToList();
+
+            var commandList = commands
+                .Select(command => $"**{command.Aliases.Aggregate((i, j) => i + " " + j)}** {command.Summary}")
+                .ToList();
+            var fieldString = commandList.Aggregate((i, j) => i + " | " + j).ToString();
+
+            var embedBuilder = new EmbedBuilder
+            {
+                Title = "Sound Commands"
+            };
+
+            embedBuilder
+                .WithDescription(fieldString)
+                .AddField("Links",
+                    "[Github](https://github.com/MoriPastaPizza/DiscordOgerBotWeb) | " +
+                    "[Lade den Bot auf deinen Server ein!](https://discord.com/api/oauth2/authorize?client_id=761895612291350538&permissions=383040&scope=bot) | " +
+                    "[DrachenlordKoreaDiscord](https://discord.gg/MmWQ5pCsHa)")
+                .WithAuthor(Context.Client.CurrentUser)
+                .WithFooter(footer =>
+                    footer.Text =
+                        Controller.OgerBot.FooterDictionary[_rand.Next(Controller.OgerBot.FooterDictionary.Count)])
+                .WithColor(Color.Red)
+                .WithCurrentTimestamp();
+
+            await Context.Channel.SendMessageAsync(embed: embedBuilder.Build());
+
+        }
     }
 }
+
