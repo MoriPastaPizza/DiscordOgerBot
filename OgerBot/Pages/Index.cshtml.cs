01cd295de53864e1a55d351f20a4b09b8798e3a5
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2020-12-12 12:09:33 +0100

    Refactor

--- OgerBot/Pages/Index.cshtml.cs
+++ OgerBot/Pages/Index.cshtml.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace DiscordOgerBotWeb.Pages
 {
     public class IndexModel : PageModel
     {
+        // ReSharper disable once NotAccessedField.Local
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
