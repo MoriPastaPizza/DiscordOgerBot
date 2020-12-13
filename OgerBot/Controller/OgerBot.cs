2b7c2950e87d58840cd9b78dc79eaeb3253c3335
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2020-12-13 01:42:45 +0100

    Update OgerBot.cs

--- OgerBot/Controller/OgerBot.cs
+++ OgerBot/Controller/OgerBot.cs
@@ -63,7 +63,7 @@ namespace DiscordOgerBotWeb.Controller
 
                 _client.Ready += () =>
                 {
-                    _client.SetGameAsync("Schmeißt grad die Brügel Naus 👊", streamUrl: null, ActivityType.CustomStatus);
+                    _client.SetGameAsync("Schmeißt grad die Brügel Naus 👊", type: ActivityType.CustomStatus);
                     _logger.LogInformation("Bot Started!");
                     return Task.CompletedTask;
                 };
