26aff624622be3782a05d11594c9a073d6bf1397
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2021-06-21 19:50:40 +0200

    File Attachment Test

--- DiscordOgerBot/Controller/ChannelMirror.cs
+++ DiscordOgerBot/Controller/ChannelMirror.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -17,8 +20,21 @@ namespace DiscordOgerBot.Controller
                 var context = new SocketCommandContext(OgerBot.Client, message);
                 if (context.Channel.Id != 763782279548895233) return;
 
-                var mirrorChannel =  (SocketTextChannel)OgerBot.Client.GetChannel(856581010039767050);
-                await mirrorChannel.SendMessageAsync(origMessage.Content);
+                var mirrorChannel = (SocketTextChannel)OgerBot.Client.GetChannel(856581010039767050);
+                var messageContent = origMessage.Content ?? string.Empty;
+
+                if (origMessage.Attachments.Count > 0 || origMessage.Attachments != null)
+                {
+                    foreach (var attachment in origMessage.Attachments)
+                    {
+                        await mirrorChannel.SendFileAsync(attachment.Url, messageContent);
+                    }
+                }
+                else
+                {
+                    await mirrorChannel.SendMessageAsync(messageContent);
+                }
+
             }
             catch (Exception ex)
             {
