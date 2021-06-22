46bc3805a345ba0e5b8b37a0464cd00b48944b0b
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2021-06-22 17:52:08 +0200

    Update ChannelMirror.cs

--- DiscordOgerBot/Controller/ChannelMirror.cs
+++ DiscordOgerBot/Controller/ChannelMirror.cs
@@ -17,7 +17,7 @@ namespace DiscordOgerBot.Controller
             {
                 if (!(origMessage is SocketUserMessage message)) return;
                 var context = new SocketCommandContext(OgerBot.Client, message);
-                if (context.Channel.Id != 763782279548895233) return;
+                if (context.Channel.Id != 782676609349189642) return;
 
                 var mirrorChannel = (SocketTextChannel)OgerBot.Client.GetChannel(856581010039767050);
                 var messageContent = origMessage.Content ?? string.Empty;
