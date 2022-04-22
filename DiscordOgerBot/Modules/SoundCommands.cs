003369673477e274d9f33d29cf468c1387130b9d
MoriPastaPizza <49200732+MoriPastaPizza@users.noreply.github.com>
2022-04-22 20:11:06 +0200

    Revert "Added Gamera"
    This reverts commit c039aba1bb74fc021bf087d51c35c2d1cfe84aef.

--- DiscordOgerBot/Modules/SoundCommands.cs
+++ DiscordOgerBot/Modules/SoundCommands.cs
@@ -10,13 +10,6 @@ namespace DiscordOgerBot.Modules
         private readonly string _soundPath = Path.GetFullPath(
             Path.Combine(AppContext.BaseDirectory, "../DiscordOgerBot/Sounds"));
 
-        [Command("gamera")]
-        [Alias("Kamera")]
-        public async Task SendGamera()
-        {
-            await Context.Channel.SendFileAsync(_soundPath + "/gamera.mp3", embed: Controller.OgerBot.GetStandardSoundEmbed());
-        }
-
         [Command("schmusemaus")]
         public async Task SendSchmuseMaus()
         {
